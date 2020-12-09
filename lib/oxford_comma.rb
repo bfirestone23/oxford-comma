0ee6736e44e43ba86cbabd6a303151973ae2a723
Brian Firestone <bfirestone2339@gmail.com>
2020-12-09 13:52:30 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.length < 3
+        return array.join(" and ")
+    else 
+        array[-1] = "and #{array[-1]}"
+    end
+    array.join(", ")
 end
